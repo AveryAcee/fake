5f632a40a71f63e23bb9f98caf4c38ff6bff4ac6
Avery Acee <146315988+AveryAcee@users.noreply.github.com>
2025-07-30 20:51:47 +0000

    Implement debounce utility

---
+++ fake_project/laravel/Controller354.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Controller354 extends Controller
+{
+    public function index()
+    {
+        return response()->json(['data' => 'Controller 354']);
+    }
+}
