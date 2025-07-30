d8696ae019583eedca8e1feec9e183836a94ffc9
Avery Acee <146315988+AveryAcee@users.noreply.github.com>
2025-07-30 20:56:45 +0000

    Write documentation for setup

---
+++ fake_project/laravel/Controller361.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Controller361 extends Controller
+{
+    public function index()
+    {
+        return response()->json(['data' => 'Controller 361']);
+    }
+}
