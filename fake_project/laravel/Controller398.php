3a6d6d07a27ae225f5e15979556cfa141204afd9
Avery Acee <146315988+AveryAcee@users.noreply.github.com>
2025-07-30 20:58:21 +0000

    Fix bug in checkout flow

---
+++ fake_project/laravel/Controller398.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Controller398 extends Controller
+{
+    public function index()
+    {
+        return response()->json(['data' => 'Controller 398']);
+    }
+}
