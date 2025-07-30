61f7d2ef52f117825958aa570b93d4ca9e7f47e8
Avery Acee <146315988+AveryAcee@users.noreply.github.com>
2025-07-30 20:47:58 +0000

    Update dependency versions

---
+++ fake_project/laravel/Controller269.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Controller269 extends Controller
+{
+    public function index()
+    {
+        return response()->json(['data' => 'Controller 269']);
+    }
+}
