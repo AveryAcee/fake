e6baf7229b6431ec6d1790fedc00c5d0ccf00e02
Avery Acee <146315988+AveryAcee@users.noreply.github.com>
2025-07-30 20:49:28 +0000

    Enhance test coverage

---
+++ fake_project/laravel/Controller300.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Controller300 extends Controller
+{
+    public function index()
+    {
+        return response()->json(['data' => 'Controller 300']);
+    }
+}
